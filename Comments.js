a0093fb7234d06185a6a8dd949668081653bc601
Binns <daniel.binns@sap.com>
2019-06-05 13:37:26 +0100

    comments.js and Use.Strict.txt added

---
+++ Comments.js
@@ -0,0 +1,15 @@
+// This comment occupies a line of its own
+
+/* An example with two messages.
+This is a multiline comment.
+*/
+
+/* Commenting out the code
+alert('Hello');
+*/
+
+/*Use hotkeys!
+In most editors, a line of code can be commented out by pressing the Ctrl+/ hotkey for a single-line comment 
+and something like Ctrl+Shift+/ – for multiline comments (select a piece of code and press the hotkey). 
+For Mac, try Cmd instead of Ctrl.
+*/
