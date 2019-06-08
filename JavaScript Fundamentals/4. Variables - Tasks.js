f0eab12f98c5c1aab6b2ba18abae76c5dd3e3ee1
DanBinns <dbinns@live.co.uk>
2019-06-08 17:33:38 +0100

    task complete

---
+++ JavaScript Fundamentals/4. Variables - Tasks.js
@@ -0,0 +1,27 @@
+/*
+Working with variables
+
+1. Declare two variables: admin and name.
+2. Assign the value "John" to name.
+3. Copy the value from name to admin.
+4. Show the value of admin using alert (must output “John”).
+*/
+
+let admin;
+let name;
+
+name = "John";
+admin = name;
+
+alert(admin);
+
+/*
+Giving the right name
+
+1. Create a variable with the name of our planet. How would you name such a variable?
+2. Create a variable to store the name of a current visitor to a website. How would you name that variable?
+*/
+
+let ourPlanetName = "Earth";
+let currentUserName = "John";
+
